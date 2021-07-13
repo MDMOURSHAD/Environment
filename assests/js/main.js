7d30790c22aff9369588979d0a3234ce51aed4b1
MDMOURSHAD <77190784+MDMOURSHAD@users.noreply.github.com>
2021-07-13 20:28:42 +0600

    Add files via upload

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,2 +1,38 @@
-/////////////////////
-////////////////////
+
+
+jQuery(document).ready(function($) {
+
+        /*-- Mobile Menu Show & Hide --*/
+
+        $(".menu-toggle-btn").click(function(){
+            $(".main-menu").toggleClass("open-nav");
+        });
+
+        /*-- Mobile Menu Show & Hide --*/
+
+        $(".open-search-box").click(function(){
+            $(".search-box").toggleClass("search-box-show");
+        });
+
+        /*-- Menu Sticky --*/
+
+        var windows = $(window);
+        var sticky = $('.header-sticky')
+        windows.on('scroll', function() {
+            var scroll = windows.scrollTop();
+            if (scroll < 250) {
+                sticky.removeClass('stick');
+            } else {
+                sticky.addClass('stick');
+            }
+        });
+
+        /*-- Counter Up --*/
+
+        $('.counter').counterUp({
+            delay: 10,
+            time: 2000
+        });
+
+            
+});
